65d5b65cd9eb098ee71519df7371a09384468291
rishit-singh <rsrishitsingh@gmail.com>
2023-02-27 00:00:22 -0800

    Initialization. && git push

---
+++ CourseDBService.cs
@@ -0,0 +1,18 @@
+namespace CourseDB.WebAPI;
+
+public class CourseDBService : IService
+{
+    protected CourseScraper Scraper;
+
+    public void Start()
+    {
+    }
+
+    public void Stop()
+    {
+    }
+
+    public CourseDBService()
+    {
+    }
+}
---
+++ IService.cs
@@ -0,0 +1,10 @@
+namespace CourseDB.WebAPI;
+
+/// <summary>
+/// Simple service interface
+/// </summary>
+public interface IService
+{
+    void Start();
+    void Stop(); 
+}
